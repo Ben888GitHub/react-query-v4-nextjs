21141c86cb89ef4229d1b144dc07b46261c00806
Benedict <benedictryan80@gmail.com>
2022-11-18 00:12:59 +0700

    implement pagination query

--- pages/[type]/popular/[page].js
+++ pages/[type]/popular/[page].js
@@ -1,9 +1,11 @@
 import { useQuery, dehydrate, QueryClient } from '@tanstack/react-query';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { Fragment } from 'react';
 import { fetchFilm } from '../../../api';
 
 function Popular({ type, page }) {
+	const router = useRouter();
 	const filmType = type === 'tv' ? 'TV Shows' : 'Movies';
 
 	const { data } = useQuery({
@@ -12,22 +14,54 @@ function Popular({ type, page }) {
 		keepPreviousData: true
 	});
 
+	console.log(data);
+	console.log(router.query);
+
 	return (
 		<>
 			<Head>
 				<title>{`Popular ${type}`}</title>
 				<meta name="description" content="popular film" />
-				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<div style={{ margin: 20, textAlign: 'center' }}>
 				<h1>{`Popular ${filmType}`}</h1>
+				<br />
+				<button
+					disabled={Number(page) === 1}
+					onClick={() => {
+						router.push({
+							pathname: `/[type]/popular/[page]`,
+							query: {
+								type,
+								page: Number(router.query.page) - 1
+							}
+						});
+					}}
+				>
+					Prev
+				</button>
+				<button
+					disabled={Number(page) === 40}
+					onClick={() =>
+						router.push({
+							pathname: `/[type]/popular/[page]`,
+							query: {
+								type,
+								page: Number(router.query.page) + 1
+							}
+						})
+					}
+				>
+					Next
+				</button>
 				<br />
 				{data?.results?.map((film, idx) => (
 					<Fragment key={idx}>
 						<h2>{film.title || film.name}</h2>
 					</Fragment>
 				))}
+				<br />
 			</div>
 		</>
 	);
